d4bb92b926b04cb52344a103fe142194a1232113
timlyeee <timelyeee@gmail.com>
2023-12-18 23:54:40 +0800

    add sqlite

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {openDatabase} from 'react-native-sqlite-storage';
+
+// 打开或创建数据库
+const db = openDatabase({ name: 'mydatabase.db', location: 'default' });
+
+// 执行 SQL 查询等操作
+db.transaction(tx => {
+  tx.executeSql('SELECT * FROM mytable;', [], (_, { rows }) => {
+    console.log(rows.raw());
+  });
+});
+
+// 关闭数据库
+db.close();
+
 
 export default function App() {
   return (
